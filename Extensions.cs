078c18593bccd5b4e8f65b5c9eb55fbd1aa2593b
implicitlycorrect <sourgreenapple@proton.me>
2024-01-07 18:21:20 +0100

    Initial commit.

---
+++ Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Serilog.Configuration;
+
+namespace Serilog.Sinks.TelegramBot;
+
+public static class Extensions
+{
+    public static LoggerConfiguration TelegramChat(this LoggerSinkConfiguration sinkConfiguration, string botToken,
+        long chatId)
+    {
+        return sinkConfiguration.Sink(new TelegramChatSink(botToken, chatId));
+    }
+}
---
+++ TelegramChatSink.cs
@@ -0,0 +1,16 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Serilog.Sinks.TelegramBot;
+
+public sealed class TelegramChatSink(string botToken, ChatId chatId) : ILogEventSink
+{
+    private readonly TelegramBotClient _botClient = new(botToken);
+
+    public void Emit(LogEvent logEvent)
+    {
+        _botClient.SendTextMessageAsync(chatId, logEvent.RenderMessage()).Wait();
+    }
+}
